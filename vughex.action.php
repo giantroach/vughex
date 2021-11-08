63e59483a460e0ccdd090b15a033d3a548c728e5
Tomoki Motohashi <tomoki.motohashi@gmail.com>
2021-11-08 22:22:37 +0900

    fix spacing

--- vughex.action.php
+++ vughex.action.php
@@ -7,7 +7,7 @@
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
  * -----
- * 
+ *
  * vughex.action.php
  *
  * Vughex main action entry point
@@ -15,54 +15,52 @@
  *
  * In this file, you are describing all the methods that can be called from your
  * user interface logic (javascript).
- *       
+ *
  * If you define a method "myAction" here, then you can call it from your javascript code with:
  * this.ajaxcall( "/vughex/vughex/myAction.html", ...)
  *
  */
-  
-  
-  class action_vughex extends APP_GameAction
-  { 
+
+
+class action_vughex extends APP_GameAction
+{
     // Constructor: please do not modify
-   	public function __default()
-  	{
-  	    if( self::isArg( 'notifwindow') )
-  	    {
+    public function __default()
+    {
+        if( self::isArg( 'notifwindow') )
+        {
             $this->view = "common_notifwindow";
-  	        $this->viewArgs['table'] = self::getArg( "table", AT_posint, true );
-  	    }
-  	    else
-  	    {
+            $this->viewArgs['table'] = self::getArg( "table", AT_posint, true );
+        }
+        else
+        {
             $this->view = "vughex_vughex";
             self::trace( "Complete reinitialization of board game" );
-      }
-  	} 
-  	
-  	// TODO: defines your action entry points there
+        }
+    }
+
+    // TODO: defines your action entry points there
 
 
     /*
-    
-    Example:
-  	
-    public function myAction()
-    {
-        self::setAjaxMode();     
 
-        // Retrieve arguments
-        // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
-        $arg1 = self::getArg( "myArgument1", AT_posint, true );
-        $arg2 = self::getArg( "myArgument2", AT_posint, true );
+      Example:
 
-        // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
-        $this->game->myAction( $arg1, $arg2 );
+      public function myAction()
+      {
+      self::setAjaxMode();
 
-        self::ajaxResponse( );
-    }
-    
+      // Retrieve arguments
+      // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
+      $arg1 = self::getArg( "myArgument1", AT_posint, true );
+      $arg2 = self::getArg( "myArgument2", AT_posint, true );
+
+      // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
+      $this->game->myAction( $arg1, $arg2 );
+
+      self::ajaxResponse( );
+      }
+
     */
 
-  }
-  
-
+}
